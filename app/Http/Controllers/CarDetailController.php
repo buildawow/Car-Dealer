928d3fb67a21581d0473f706abf38255cc48d84c
ArielSalvadorDev <arielsalvadormejia@gmail.com>
2019-11-12 20:03:45 -0600

    Hotfix on typo App\Car as App\car

--- app/Http/Controllers/CarDetailController.php
+++ app/Http/Controllers/CarDetailController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\car;
+use App\Car;
 use Illuminate\Http\Request;
 
 class CarDetailController extends Controller
@@ -41,7 +41,7 @@ class CarDetailController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\car  $car
+     * @param  \App\Car  $car
      * @return \Illuminate\Http\Response
      */
     public function show(car $car)
@@ -55,7 +55,7 @@ class CarDetailController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\car  $car
+     * @param  \App\Car  $car
      * @return \Illuminate\Http\Response
      */
     public function edit(car $car)
@@ -67,7 +67,7 @@ class CarDetailController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\car  $car
+     * @param  \App\Car  $car
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, car $car)
@@ -78,7 +78,7 @@ class CarDetailController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\car  $car
+     * @param  \App\Car  $car
      * @return \Illuminate\Http\Response
      */
     public function destroy(car $car)
